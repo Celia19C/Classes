df46559cb6923d75ee87da4a0f8a0a515204416c
Celia Conde <celia_conde@yahoo.es>
2018-08-27 19:52:21 +0200

    ejer SuperBall

---
+++ ejerSuperBall.js
@@ -0,0 +1,33 @@
+// Regular Ball Super Ball
+// Create a class Ball.
+
+// Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+
+
+
+
+class Ball {
+    constructor(name) {
+      this.name = name || 'regular'
+    }
+  
+    get ballType() {
+      return this.name
+      }
+  }
+
+
+  //OTRA FROMA
+
+
+  var Ball = function(ballType) {
+    this.ballType = ballType || 'regular';
+  };
